322afcc3d34b0866e3374cd5e7bf8ec0c9b68a08
osy <ayul5@naver.com>
2021-07-19 03:46:27 +0900

    Design: Change remove button
    - svg 코드 인라인 방식에서 css의 마스크와 배경 색상 속성을 이용해
      재활용하도록 변경함

--- js/item/todo.item.js
+++ js/item/todo.item.js
@@ -2,10 +2,12 @@ export const TodoItem = (content) => {
   // 요소 생성
   const wrap = document.createElement("div"),
     checkboxInput = document.createElement("input"),
-    checkboxLabel = document.createElement("label");
+    checkboxLabel = document.createElement("label"),
+    removeButton = document.createElement("button");
 
   // 클래스 추가
   wrap.classList.add("item");
+  removeButton.classList.add("item__button--remove");
 
   // 속성값 설정
   checkboxInput.type = "checkbox";
@@ -18,6 +20,7 @@ export const TodoItem = (content) => {
   // 요소 연결
   wrap.appendChild(checkboxInput);
   wrap.appendChild(checkboxLabel);
+  wrap.appendChild(removeButton);
 
   return wrap;
 };
